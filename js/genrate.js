ea0519ae6d680daf40fbfbd1344586f695229bd5
mbatwc113114 <95271461+mbatwc113114@users.noreply.github.com>
2024-09-23 01:14:21 +0530

    test

--- js/genrate.js
+++ js/genrate.js
@@ -4,19 +4,34 @@ var textBox = document.getElementById("Text");
 var qr;
 var qrComp = document.getElementById("qrcode");
 
-var productName =document.getElementById("productName");
-var productId =document.getElementById("productID");
-var material =document.getElementById("material");
-var Discription =document.getElementById("Discription");
-var catagory =document.getElementById("catagory");
+var Status = document.getElementById("status");
+var downloadBtn = document.getElementById("download");
 
+const productName =document.getElementById("productName");
+const productId =document.getElementById("productId");
+const material =document.getElementById("material");
+const catagory =document.getElementById("catagory");
+const Discription =document.getElementById("Discription");
+
+
+let inputBoxs = document.querySelectorAll(".form input")
+
+downloadBtn.addEventListener("click", ()=>{
+    const url = qrComp.scr;
+    const a = document.createElement('a');
+    a.href = imageUrl;
+    a.download = `${productName}-qr.jpg`; 
+    a.click();
+})
 
 function qrGenrate(){
-    var data = `product Id ${productId} \n
-    product Name ${productName} \n
-    material ${material} \n
-    Discription ${Discription} \n
-    catagory ${catagory} \n
+    
+    
+    var data = `product Id ${productId.value} \n
+    product Name ${productName.value} \n
+    material ${material.value} \n
+    Discription ${Discription.value} \n
+    catagory ${catagory.value} \n
     ` 
 
 if(qrComp.firstChild!=null){
@@ -24,17 +39,44 @@ if(qrComp.firstChild!=null){
     qrComp.removeChild(qrComp.firstChild);
 }
 }
-    qr = new QRCode("qrcode", {
-        text: tex,
+try{
+    if(qrComp.classList.value == 'hide'){
+        qrComp.classList.remove('hide');
         
+    }
+    if(Status.classList.value == ''){
+        Status.classList.add('hide');
+        
+    }
+    qr = new QRCode("qrcode", {
+
+        text: data,
+        version:2,
         width: 256,
         height: 256,
         colorDark : "#000000",
         colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
        });
+}
+catch(error){
+    
+    if(qrComp.classList.value == ''){
+        qrComp.classList.add('hide');
+    }
+    if(Status.classList.value == 'hide'){
+        Status.classList.remove('hide');
+    }
+}
       
     }
+
+
+
+inputBoxs.forEach((input)=>{
+    input.addEventListener("input",qrGenrate);
+})
+
 
 
 
